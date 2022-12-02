966eccd59bb5a117829fe9d2da97a7e591325ac7
Kicamoo <Kicamoo@gmail.com>
2022-12-02 20:57:34 +0900

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,9 @@ import { HealthcheckController } from './healthcheck/healthcheck.controller';
 
 import { SocketModule } from './socket/socket.module';
 import { LoggerModule } from './logger/logger.module';
-// import { PrematchRmqModule } from './prematchRmq/rmq.module';
-// import { InplayRmqModule } from './inplayRmq/rmq.module';
 
 @Module({
-  imports: [LoggerModule, SocketModule /*PrematchRmqModule, InplayRmqModule*/],
+  imports: [LoggerModule, SocketModule],
   providers: [],
   controllers: [HealthcheckController],
 })
--- src/socket/socket.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Server } from 'ws';
-
-@Injectable()
-export class SocketService {
-  public socket: Server = null;
-}
